4c6461dc03c105d83398896b287d0c67ccf1ed6d
Ahmed Ramy <dev.ahmedramy@gmail.com>
2022-08-21 08:18:21 +0200

    Move things around and polish the marks for better display

--- ErrorHandling/Modules/Login/Providers/Apple/AppleLoginViewModel.swift
+++ ErrorHandling/Modules/Login/Providers/Apple/AppleLoginViewModel.swift
@@ -8,44 +8,13 @@
 import AuthenticationServices
 import Utils
 
-// MARK: - AppleSignInError
-
-struct AppleSignInError: PresentationError {
-  let title: String
-  let description: String?
-  let type: PresentationMethod
-  let icon: UIImage?
-
-  init(title: String, description: String? = nil, type: PresentationMethod = .indicator, icon: UIImage? = nil) {
-    self.title = title
-    self.description = description
-    self.type = type
-    self.icon = icon
-  }
-}
-
 // MARK: - AppleSignInViewModel
 
-class AppleSignInViewModel {
-  // MARK: Lifecycle
-
-  init() { }
-
-  init(uiDelegate: AppleAuthUIDelegateProtocol) {
-    self.uiDelegate = uiDelegate
-  }
-
+public class AppleSignInViewModel {
   // MARK: Internal
 
-  @Published var state: State = .idle
-
-  func attemptSignIn() {
-    let request = ASAuthorizationAppleIDProvider().createRequest().then { $0.requestedScopes = [.fullName] }
-    let authController = ASAuthorizationController(authorizationRequests: [request])
-    authController.delegate = uiDelegate
-    authController.performRequests()
-  }
-
+  @Published public var state: State = .idle
+  
   // MARK: Private
 
   private lazy var uiDelegate: AppleAuthUIDelegateProtocol = AppleAuthUIDelegate(
@@ -59,13 +28,28 @@ class AppleSignInViewModel {
 
   private lazy var onErrorHandler: Callback<ASAuthorizationError> = { [weak self] error in
     guard let self = self else { return }
-    self.state = .failure(
-      AppleSignInError(title: error.localizedDescription))
+    guard error.code != .canceled else { return }
+    self.state = .failure(.init(error))
+  }
+  
+  // MARK: Lifecycle
+
+  public init() { }
+
+  public init(uiDelegate: AppleAuthUIDelegateProtocol) {
+    self.uiDelegate = uiDelegate
+  }
+
+  public func attemptSignIn() {
+    let request = ASAuthorizationAppleIDProvider().createRequest().then { $0.requestedScopes = [.fullName] }
+    let authController = ASAuthorizationController(authorizationRequests: [request])
+    authController.delegate = uiDelegate
+    authController.performRequests()
   }
 }
 
 extension AppleSignInViewModel {
-  enum State {
+  public enum State {
     case idle
     case success(SuccessMessage)
     case failure(AppleSignInError)
@@ -82,17 +66,13 @@ public protocol AppleAuthUIDelegateProtocol: ASAuthorizationControllerDelegate {
 // MARK: - AppleAuthUIDelegate
 
 public final class AppleAuthUIDelegate: NSObject, AppleAuthUIDelegateProtocol {
-  // MARK: Lifecycle
+  public var onSuccess: VoidCallback
+  public var onError: Callback<ASAuthorizationError>
 
   public init(onSuccess: @escaping VoidCallback, onError: @escaping Callback<ASAuthorizationError>) {
     self.onSuccess = onSuccess
     self.onError = onError
   }
-
-  // MARK: Public
-
-  public var onSuccess: VoidCallback
-  public var onError: Callback<ASAuthorizationError>
 
   public func authorizationController(controller _: ASAuthorizationController, didCompleteWithAuthorization _: ASAuthorization) {
     onSuccess()
@@ -105,5 +85,16 @@ public final class AppleAuthUIDelegate: NSObject, AppleAuthUIDelegateProtocol {
     }
 
     onError(error)
+  }
+}
+
+extension AppleSignInError {
+  init(_ authError: ASAuthorizationError) {
+    switch authError.code {
+    case .unknown:
+      self = .init(title: "Something went wrong", description: "with an unknown reason")
+    default:
+      self = .init(title: "Sign in failed")
+    }
   }
 }
